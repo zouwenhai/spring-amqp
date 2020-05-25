fa6785bbfc7403f5c3e8bec97606a8467a4ba15a
1466625029@qq.com <zou19941205>
2020-05-25 15:19:22 +0800

    spring-amqp

--- src/main/java/com/example/consumer/Consumer.java
+++ src/main/java/com/example/consumer/Consumer.java
@@ -1,6 +1,7 @@
 package com.example.consumer;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
 
 /**
@@ -17,4 +18,14 @@ public class Consumer {
     public void processMessage(String msg) {
         System.out.format("Receiving Message: -----[%s]----- \n.", msg);
     }
+
+
+    @RabbitListener(queues = "cord")
+    @SendTo("cord")
+    //@RabbitListener(queues = "cord", containerFactory="myFactory")
+    public String  processMessage2(String msg) {
+        System.out.format("Receiving Message: -----[%s]----- \n.", msg);
+        return "已经接收到信息";
+    }
+
 }
